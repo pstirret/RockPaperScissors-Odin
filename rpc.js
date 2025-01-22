0631431d433ecbc6fe2ef288bc6faaf76814acc7
Prakash Stirret <102489686+pstirret@users.noreply.github.com>
2025-01-21 21:31:33 -0800

    Commit final code for Rock, Paper, Scissors game

--- rpc.js
+++ rpc.js
@@ -1,6 +1,89 @@
-console.log("Hello World!");
 
-console.log(getComputerChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
+
+
+function playGame (){
+   /**
+    * Initialize scores to 0
+    * Loop 5 times
+    *    get selections
+    *    call playRound function
+    * Determine the winner
+    * Declare winner in output
+    */
+   for(let i=0; i<5; i++) {
+      const humanSelection = getHumanChoice();
+      const computerSelection = getComputerChoice();
+
+      playRound(humanSelection, computerSelection);
+
+      console.log(`The score is: You - ${humanScore}, Computer - ${computerScore}`)
+   }
+   
+   let scoreStr = `The final scores are: You - ${humanScore}, the Computer - ${computerScore}`;
+
+   if (humanScore > computerScore) {
+      console.log(`You are the winner! ${scoreStr}`);
+   }
+   else if (computerScore > humanScore)
+      console.log(`You lose. ${scoreStr}`);
+   else {
+      console.log(`It's a tie! ${scoreStr}`);
+   }
+}
+
+
+function playRound (humanChoice, computerChoice) {
+   let roundStr = `${humanChoice}-${computerChoice}`;
+   let result = "";
+
+   switch(roundStr) {
+      case `Rock-Rock`:
+         result = `tie`
+         break;
+      case `Rock-Paper`:
+         result = `computer`
+         break;
+      case `Rock-Scissors`:
+         result = `human`
+         break;
+      case `Paper-Rock`:
+         result = `human`
+         break;   
+      case `Paper-Paper`:
+         result = `tie`
+         break;
+      case `Paper-Scissors`:
+         result = `computer`
+         break;
+      case `Scissors-Rock`:
+         result = `computer`
+         break;
+      case `Scissors-Paper`:
+         result = `human`
+         break;
+      case `Scissors-Scissors`:
+         result = `tie`
+         break;   
+   }
+
+   let picksStr = `You picked ${humanChoice} and the Computer picked ${computerChoice}`;
+
+   if (result === `tie`){
+      console.log(`It's a tie. ${picksStr}`);
+   }
+   else if(result === `human`) {
+      console.log(`You win! ${picksStr}`);
+      humanScore++;
+   }
+   else {
+      console.log(`You lose. ${picksStr}`);
+      computerScore++;
+   }
+}
 
 function getComputerChoice() {
    /**
@@ -26,5 +109,36 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-   
+   /**
+    * Create prompt messages
+    * Set loop exit flag to false
+    * WHILE not loop exit
+    *    prompt user for input
+    *    validate input
+    *    IF input is valid
+    *       set loop exit flag
+    *    ELSE
+    *       Display error message
+    *    END
+    * END-WHILE
+    * 
+    * RETURN user input
+    */
+
+   let promptString = 'Please enter your selection: Rock, Paper, Scissors';
+
+   let errorString = 'Your entry must be either Rock, Paper, or Scissors'
+
+   while(true) {
+      let userInput = prompt(promptString, "");
+      let tmpStr = userInput.toLowerCase();
+
+      if (tmpStr !== "rock" && tmpStr !== "paper" && tmpStr !==         "scissors") {
+         console.log("Incorrect entry. You must enter either \"Rock\", \"Paper\", or \"Scissors\"");
+      }
+      else {
+         return tmpStr.charAt(0).toUpperCase() + tmpStr.slice(1);
+      }  
+   }
+
 }
